f26ae6e616f669c29e70a0039edb1bad742093ee
tahir-bashir09 <73584198+tahir-bashir09@users.noreply.github.com>
2020-12-01 22:15:59 +0100

    Update PfeApplicationTests.java

--- src/test/java/be/ipl/pfe/PfeApplicationTests.java
+++ src/test/java/be/ipl/pfe/PfeApplicationTests.java
@@ -9,9 +9,10 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest(classes = PfeApplication.class)
 class PfeApplicationTests {
 
-	@Test
+	/*@Test
 	void contextLoads() {
-	}
+
+	}*/
 
 	@Test
 	public void test1() {
