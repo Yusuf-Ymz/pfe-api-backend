d6beeafa248de55989315cdaba111a08793b51f3
tahir-bashir09 <73584198+tahir-bashir09@users.noreply.github.com>
2020-12-02 23:20:07 +0100

    Update CorsConfig.java

--- src/main/java/be/ipl/pfe/configs/CorsConfig.java
+++ src/main/java/be/ipl/pfe/configs/CorsConfig.java
@@ -16,7 +16,7 @@ public class CorsConfig {
                 registry.addMapping("/**")
                     .allowedMethods("GET", "POST", "PUT", "DELETE")
                     .allowedHeaders("*")
-                    .allowedOrigins("https://pfe-web-frontend.herokuapp.com", "localhost:4200");
+                    .allowedOrigins("https://pfe-web-frontend.herokuapp.com", "http://localhost:4200");
             }
         };
     }
