8ea7bea5a96aae7802212b57c8882d3b186bcbc1
antoine-hnkx <antoine.honinckx@gmail.com>
2020-12-08 02:15:26 +0100

    Added amount to qrcode generation for doctors

--- src/main/java/be/ipl/pfe/controllers/QRCodeController.java
+++ src/main/java/be/ipl/pfe/controllers/QRCodeController.java
@@ -1,40 +1,38 @@
 package be.ipl.pfe.controllers;
 
+import be.ipl.pfe.exceptions.InvalidParameterException;
 import be.ipl.pfe.ports.IdGenerator;
 import be.ipl.pfe.services.AuthService;
 import be.ipl.pfe.services.QRCodeService;
-import be.ipl.pfe.utils.JsonUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
-import java.util.Map;
+import java.util.List;
 
 @RequestMapping("/doctors/qrcodes")
 @RestController
 public class QRCodeController {
 
-    @Autowired
-    private QRCodeService qrCodeService;
+	@Autowired
+	private QRCodeService qrCodeService;
 
-    @Autowired
-    private AuthService authService;
+	@Autowired
+	private AuthService authService;
 
-    @Autowired
-    @Qualifier("UuidGenerator")
-    private IdGenerator idGenerator;
+	@Autowired
+	@Qualifier("UuidGenerator")
+	private IdGenerator idGenerator;
 
-    @PostMapping(produces = MediaType.APPLICATION_JSON_VALUE)
-    public Map<String, String> generateQRCode(@Valid @RequestHeader("Authorization") String token) {
-        String doctorId = this.authService.checkIfDoctor(token);
-        String qrCodeId = this.idGenerator.generate();
-
-        return JsonUtils.stringToJson("content", this.qrCodeService.generate(doctorId, qrCodeId));
-    }
+	@PostMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+	public List<String> generateQRCode(@RequestParam(value = "amount") Integer amount, @Valid @RequestHeader("Authorization") String token) {
+		if (amount == null || amount <= 0 || amount > 20)
+			throw new InvalidParameterException("amount", "non-null integer, between 1 and 20.");
+		String doctorId = this.authService.checkIfDoctor(token);
+		String qrCodeId = this.idGenerator.generate();
+		return this.qrCodeService.generate(amount, doctorId, qrCodeId);
+	}
 
 }
--- src/main/java/be/ipl/pfe/services/QRCodeService.java
+++ src/main/java/be/ipl/pfe/services/QRCodeService.java
@@ -3,11 +3,17 @@ package be.ipl.pfe.services;
 import be.ipl.pfe.utils.JwtUtils;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Service
 public class QRCodeService {
 
-    public String generate(String doctorId, String qrCodeId) {
-        return JwtUtils.createQRCodeContentToken(doctorId, qrCodeId);
-    }
+	public List<String> generate(Integer amount, String doctorId, String qrCodeId) {
+		List<String> hashes = new ArrayList<>();
+		for (int i = 0; i < amount; i++)
+			hashes.add(JwtUtils.createQRCodeContentToken(doctorId, qrCodeId));
+		return hashes;
+	}
 
 }
